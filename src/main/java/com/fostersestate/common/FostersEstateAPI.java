94af68b54911de788b409b7288f5e9cda6a18fce
Drew <drfalcoew@gmail.com>
2023-12-19 20:39:16 -0800

    CORS update

--- src/main/java/com/fostersestate/common/FostersEstateAPI.java
+++ src/main/java/com/fostersestate/common/FostersEstateAPI.java
@@ -1,8 +1,12 @@
 package com.fostersestate.common;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class FostersEstateAPI extends Application {
+    public FostersEstateAPI() {
+        Log.debug("FostersEstateAPI started");
+    }
 }
